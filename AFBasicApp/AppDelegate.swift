05aff6743aaf679c3b515ffc1ffac5eee83dde14
Oded <oded.rinsky@appsflyer.com>
2021-06-10 14:06:50 +0300

    Integrated the AppsFlyer SDK

--- AFBasicApp/AppDelegate.swift
+++ AFBasicApp/AppDelegate.swift
@@ -1,36 +1,112 @@
-//
-//  AppDelegate.swift
-//  AFBasicApp
-//
-//  Created by Oded Rinsky on 10/06/2021.
-//
-
 import UIKit
+import AppsFlyerLib
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    var window: UIWindow?
+    let defaults = UserDefaults.standard
+    //MARK: LifeCycle
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // 1 - Get AppsFlyer preferences from .plist file
+        guard let propertiesPath = Bundle.main.path(forResource: "afdevkey_donotpush", ofType: "plist"),
+            let properties = NSDictionary(contentsOfFile: propertiesPath) as? [String:String] else {
+                fatalError("Cannot find `afdevkey_donotpush`")
+        }
+        guard let appsFlyerDevKey = properties["appsFlyerDevKey"],
+                   let appleAppID = properties["appleAppID"] else {
+            fatalError("Cannot find `appsFlyerDevKey` or `appleAppID` key")
+        }
+        // 2 - Replace 'appsFlyerDevKey', `appleAppID` with your DevKey, Apple App ID
+        AppsFlyerLib.shared().appsFlyerDevKey = appsFlyerDevKey
+        AppsFlyerLib.shared().appleAppID = appleAppID
+        
+        AppsFlyerLib.shared().delegate = self
+        AppsFlyerLib.shared().isDebug = true
+        // iOS 10 or later
+        if #available(iOS 10, *) {
+            UNUserNotificationCenter.current().requestAuthorization(options: [.badge, .alert, .sound]) { _, _ in }
+            application.registerForRemoteNotifications()
+        }
+        // iOS 9 support - Given for reference. This demo app supports iOS 13 and above
+        else {
+            application.registerUserNotificationSettings(UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil))
+            application.registerForRemoteNotifications()
+        }
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Start the SDK (start the IDFA timeout set above, for iOS 14 or later)
+        AppsFlyerLib.shared().start()
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    // Open Univerasal Links
+    // For Swift version < 4.2 replace function signature with the commented out code:
+    // func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+        print(" user info \(userInfo)")
+        AppsFlyerLib.shared().handlePushNotification(userInfo)
     }
-
-
+    // Open Deeplinks
+    // Open URI-scheme for iOS 8 and below
+    private func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+        AppsFlyerLib.shared().continue(userActivity, restorationHandler: restorationHandler)
+        return true
+    }
+    // Open URI-scheme for iOS 9 and above
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        AppsFlyerLib.shared().handleOpen(url, sourceApplication: sourceApplication, withAnnotation: annotation)
+        return true
+    }
+    // Report Push Notification attribution data for re-engagements
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        AppsFlyerLib.shared().handleOpen(url, options: options)
+        return true
+    }
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        AppsFlyerLib.shared().handlePushNotification(userInfo)
+    }
+    // Reports app open from deep link for iOS 10 or later
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        AppsFlyerLib.shared().continue(userActivity, restorationHandler: nil)
+        return true
+    }
 }
-
+//MARK: AppsFlyerLibDelegate
+extension AppDelegate: AppsFlyerLibDelegate{
+    // Handle Organic/Non-organic installation
+    func onConversionDataSuccess(_ installData: [AnyHashable: Any]) {
+        print("onConversionDataSuccess data:")
+        for (key, value) in installData {
+            print(key, ":", value)
+        }
+        if let status = installData["af_status"] as? String {
+            if (status == "Non-organic") {
+                if let sourceID = installData["media_source"],
+                    let campaign = installData["campaign"] {
+                    print("This is a Non-Organic install. Media source: \(sourceID)  Campaign: \(campaign)")
+                }
+            } else {
+                print("This is an organic install.")
+            }
+            if let is_first_launch = installData["is_first_launch"] as? Bool,
+                is_first_launch {
+                print("First Launch")
+            } else {
+                print("Not First Launch")
+            }
+        }
+    }
+    func onConversionDataFail(_ error: Error) {
+        print(error)
+    }
+    //Handle Deep Link
+    func onAppOpenAttribution(_ attributionData: [AnyHashable : Any]) {
+        //Handle Deep Link Data
+        print("onAppOpenAttribution data:")
+        for (key, value) in attributionData {
+            print(key, ":",value)
+        }
+    }
+    func onAppOpenAttributionFailure(_ error: Error) {
+        print(error)
+    }
+}
